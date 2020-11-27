5cf72aa7a28ae5f85dfb22a49365d3b3029ec94a
Maria Jose <beche_1999@live.com.mx>
2020-11-27 17:24:32 -0600

    Navigation Drawer Activity

---
+++ app/src/main/java/com/example/faketagram_app/UploadPhoto.java
@@ -0,0 +1,14 @@
+package com.example.faketagram_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
