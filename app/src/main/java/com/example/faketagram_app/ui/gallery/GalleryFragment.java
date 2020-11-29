229c539bd341f9bffff6b2551fcdfc2f955bf872
BRUNO ANTONIO LOPEZ LUJAN <zS17021850@estudiantes.uv.mx>
2020-11-28 21:15:45 -0600

    Avance navegabilidad y refactorización

--- app/src/main/java/com/example/faketagram_app/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/faketagram_app/ui/gallery/GalleryFragment.java
@@ -6,14 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.faketagram_app.R;
 import com.example.faketagram_app.UploadPhoto;
@@ -25,7 +20,7 @@ public class GalleryFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        vista=inflater.inflate(R.layout.fragment_gallery,container,false);
+        vista=inflater.inflate(R.layout.fragment_my_profile,container,false);
         btnAddPhoto= (Button) vista.findViewById(R.id.btnAddPhoto);
 
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
