207b53a4c18384e321aee939612c52d27485f6cf
Maria Jose <beche_1999@live.com.mx>
2020-11-30 19:44:12 -0600

    Implementación evento SignIn

--- app/src/main/java/com/example/faketagram_app/SignIn.java
+++ app/src/main/java/com/example/faketagram_app/SignIn.java
@@ -2,13 +2,29 @@ package com.example.faketagram_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SignIn extends AppCompatActivity {
+
+    View vista;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnSignIn= (Button) findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignIn.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
