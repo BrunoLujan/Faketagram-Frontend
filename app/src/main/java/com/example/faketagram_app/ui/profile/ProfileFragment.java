05f275c126458cb9c7cf7c796a32cd7898d35c88
BRUNO ANTONIO LOPEZ LUJAN <zS17021850@estudiantes.uv.mx>
2020-12-23 00:53:04 -0600

    Implementación funcional de carga de imagen y datos en My Profile

--- app/src/main/java/com/example/faketagram_app/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/faketagram_app/ui/profile/ProfileFragment.java
@@ -1,30 +1,44 @@
 package com.example.faketagram_app.ui.profile;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.faketagram_app.Constant;
 import com.example.faketagram_app.EditProfile;
 import com.example.faketagram_app.R;
 import com.example.faketagram_app.UploadPhoto;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
 
-    View vista;
+    View viewFragment;
+
+    ImageView ivImageMyProfile;
+    TextView txtNamesProfileFragment, txtStatusProfileFragment;
     Button btnAddPhoto;
     Button btnEditProfile;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        vista=inflater.inflate(R.layout.fragment_my_profile,container,false);
-        btnAddPhoto= (Button) vista.findViewById(R.id.btnAddPhoto);
-        btnEditProfile= (Button) vista.findViewById(R.id.btnEditProfile);
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
+                              Bundle savedInstanceState) {
+        viewFragment=inflater.inflate(R.layout.fragment_my_profile,container,false);
+
+        ivImageMyProfile = (ImageView) viewFragment.findViewById(R.id.ivImageMyProfile);
+        txtNamesProfileFragment = (TextView) viewFragment.findViewById(R.id.txtNamesMyProfile);
+        txtStatusProfileFragment = (TextView) viewFragment.findViewById(R.id.txtStatusMyProfile);
+
+        btnAddPhoto= (Button) viewFragment.findViewById(R.id.btnAddPhoto);
+        btnEditProfile= (Button) viewFragment.findViewById(R.id.btnEditProfile);
 
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +55,22 @@ public class ProfileFragment extends Fragment {
                 getActivity().startActivity(intent);
             }
         });
-         return vista;
 
+        setUserOnProfile();
+        return viewFragment;
     }
 
+    public void setUserOnProfile() {
+        if (Constant.LOGGEDUSER.getImage_storage_path() != null) {
+            Picasso.get().load(Constant.PROFILEIMAGE + Constant.LOGGEDUSER.getImage_storage_path()).fit().into(ivImageMyProfile);
+        }
+
+        txtNamesProfileFragment.setText(Constant.LOGGEDUSER.getName() + " " + Constant.LOGGEDUSER.getLastname());
+
+        if (Constant.LOGGEDUSER.getStatus() != null) {
+            txtStatusProfileFragment.setText("'" + Constant.LOGGEDUSER.getStatus() + "'");
+        } else {
+            txtStatusProfileFragment.setText("");
+        }
+    }
 }
